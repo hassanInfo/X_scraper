6421e7363e4484a0abe78a9acc7815909ef6065a
Hassan Id Mansour <85229840+hassanInfo@users.noreply.github.com>
2023-12-29 15:31:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser(description='Twitter Scraper')
     parser.add_argument('--keywords', type=str, help='Search keywords for tweets')
-    parser.add_argument('--n_tweets', type=int, default=4, help='Number of tweets to retrieve (default: 4)')
-    parser.add_argument('--start_date', type=str, help='Start date for retrieving tweets (YYYY-MM-DD)')
+    parser.add_argument('--n_tweets', type=int, default=300, help='Number of tweets to retrieve (default: 300)')
 
     return parser.parse_args()
 
@@ -25,9 +24,9 @@ if __name__ == "__main__":
     keywords = args.keywords.split(',') if args.keywords else []
     # Create an instance of the TwitterScraper class
     for k in keywords:
-        twitter_scraper = TwitterScraper(k, args.n_tweets, args.start_date)
+        twitter_scraper = TwitterScraper(k, args.n_tweets)
         # Scrape tweets
         twitter_scraper.scrape_tweets() 
         twitter_scraper.save_data()
 
-    merge_csv_files('./data', 'X_data')
+    merge_csv_files('./data', 'X_data.csv')
